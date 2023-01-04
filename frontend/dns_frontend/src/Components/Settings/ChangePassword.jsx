49d0c75c5e6497280d833f2a1df4b8bd434a316f
AankitP <2001aankit@gmail.com>
2023-01-03 19:43:59 -0600

    Fixed Seth's merge

--- frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
+++ frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
@@ -1,8 +1,6 @@
-import { Button, FormControlLabel, Radio, RadioGroup, Typography, useTheme } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import React from 'react';
-import SquareIcon from '@mui/icons-material/Square';
-import themes from '../../themes';
-import { getThemeStorage, setThemeStroage } from '../../scripts/getsetThemeLocal';
+import InputField from '../InputField';
 
 
 const ChangePassword = () => {
@@ -13,22 +11,32 @@ const ChangePassword = () => {
                     fontSize: 25,
                 }}
             >
-                Theme Selector
+                Change Password
             </Typography>
-            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexGrow: 1, }} >
-                <RadioGroup >
-                    {themeSelectionRows}
-                </RadioGroup>
-                
-            </div>
-
-            <Button
-                variant={'contained'}
-                sx={{  position: 'sticky', bottom: 0, }}
-                onClick={ () => handleApplyChanges() }
-            >
-                Apply Changes
-            </Button>  
+            <div class="InsideComponents" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexGrow: 1}}>
+                <div style={{ display: 'flex', flexDirection: 'column'}}>
+                    <InputField
+                        placeholder='Enter Password'
+                        label='Old Password'
+                    />
+                    <InputField
+                        placeholder='Enter Password'
+                        label='New Password'
+                        sx={{ marginTop:5 }}
+                    />
+                    <InputField
+                        placeholder='Enter Password'
+                        label='Confirm Password'
+                    />
+                    <Button
+                        variant={'contained'}
+                        sx={{  position: 'sticky', bottom: 0, marginTop: "2rem"}}
+                        // onClick={ () => handleApplyChanges() }
+                    >
+                        Apply Changes
+                    </Button>
+                </div>
+            </div>  
         </>
     );
 };
