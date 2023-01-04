c426e707357b2871e740198138877cd840b105d2
AankitP <2001aankit@gmail.com>
2023-01-03 19:45:00 -0600

    Code Formatting

--- frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
+++ frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
@@ -1,44 +1,45 @@
-import { Typography, Button } from '@mui/material';
-import React from 'react';
-import InputField from '../InputField';
-
+import { Typography, Button } from "@mui/material";
+import React from "react";
+import InputField from "../InputField";
 
 const ChangePassword = () => {
-    return (
-        <>
-            <Typography
-                sx={{ 
-                    fontSize: 25,
-                }}
-            >
-                Change Password
-            </Typography>
-            <div class="InsideComponents" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexGrow: 1}}>
-                <div style={{ display: 'flex', flexDirection: 'column'}}>
-                    <InputField
-                        placeholder='Enter Password'
-                        label='Old Password'
-                    />
-                    <InputField
-                        placeholder='Enter Password'
-                        label='New Password'
-                        sx={{ marginTop:5 }}
-                    />
-                    <InputField
-                        placeholder='Enter Password'
-                        label='Confirm Password'
-                    />
-                    <Button
-                        variant={'contained'}
-                        sx={{  position: 'sticky', bottom: 0, marginTop: "2rem"}}
-                        // onClick={ () => handleApplyChanges() }
-                    >
-                        Apply Changes
-                    </Button>
-                </div>
-            </div>  
-        </>
-    );
+  return (
+    <>
+      <Typography
+        sx={{
+          fontSize: 25,
+        }}
+      >
+        Change Password
+      </Typography>
+      <div
+        class="InsideComponents"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          flexGrow: 1,
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <InputField placeholder="Enter Password" label="Old Password" />
+          <InputField
+            placeholder="Enter Password"
+            label="New Password"
+            sx={{ marginTop: 5 }}
+          />
+          <InputField placeholder="Enter Password" label="Confirm Password" />
+          <Button
+            variant={"contained"}
+            sx={{ position: "sticky", bottom: 0, marginTop: "2rem" }}
+            // onClick={ () => handleApplyChanges() }
+          >
+            Apply Changes
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ChangePassword;
