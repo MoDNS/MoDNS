4cf1cfcf7658a051745d0189dfa89c47a83a7e05
AankitP <2001aankit@gmail.com>
2023-01-03 18:50:26 -0600

    did stuff for change password

--- frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
+++ frontend/dns_frontend/src/Components/Settings/ChangePassword.jsx
@@ -1,9 +1,35 @@
+import { Button, FormControlLabel, Radio, RadioGroup, Typography, useTheme } from '@mui/material';
 import React from 'react';
+import SquareIcon from '@mui/icons-material/Square';
+import themes from '../../themes';
+import { getThemeStorage, setThemeStroage } from '../../scripts/getsetThemeLocal';
+
 
 const ChangePassword = () => {
     return (
-        <div>
-        </div>
+        <>
+            <Typography
+                sx={{ 
+                    fontSize: 25,
+                }}
+            >
+                Theme Selector
+            </Typography>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexGrow: 1, }} >
+                <RadioGroup >
+                    {themeSelectionRows}
+                </RadioGroup>
+                
+            </div>
+
+            <Button
+                variant={'contained'}
+                sx={{  position: 'sticky', bottom: 0, }}
+                onClick={ () => handleApplyChanges() }
+            >
+                Apply Changes
+            </Button>  
+        </>
     );
 };
 
