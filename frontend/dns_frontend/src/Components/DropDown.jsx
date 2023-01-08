4900f3a8606438f277ae269c3917cfebdb3ab90e
AankitP <2001aankit@gmail.com>
2023-01-08 16:29:10 -0600

    Image
    Attempt to get image to work in button

--- frontend/dns_frontend/src/Components/DropDown.jsx
+++ frontend/dns_frontend/src/Components/DropDown.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Hidden } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 import Title from "./Title";
 import { useState } from 'react';
