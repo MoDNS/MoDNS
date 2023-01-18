5d35444e735e2bd2a9b48b68244cc50b267cadfb
AankitP <2001aankit@gmail.com>
2023-01-18 09:05:58 -0600

    One Hook
    -DropDown.jsx Uses one hook now

--- frontend/dns_frontend/src/Components/DropDown.jsx
+++ frontend/dns_frontend/src/Components/DropDown.jsx
@@ -1,81 +1,72 @@
 import { Box, Button } from "@mui/material";
 import React from "react";
 import Title from "./Title";
-import { useState } from 'react';
-import DropDownButtonImg from './Images/DropDownButton.png'
-import DropDownButtonImgInverted from './Images/DropDownButtonInverted.png'
+import { useState } from "react";
+import DropDownButtonImg from "./Images/DropDownButton.png";
+import DropDownButtonImgInverted from "./Images/DropDownButtonInverted.png";
 
 const DropDown = ({ children, sx, title, divider, titleCentered }) => {
-        
-    const Vis = ['visible', 'auto', DropDownButtonImgInverted];
-    const Hid = ['hidden', '0', DropDownButtonImg];
+  const Vis = ["visible", "auto", DropDownButtonImgInverted];
+  const Hid = ["hidden", "0", DropDownButtonImg];
 
-    const [box, setBox] = useState(0);
+  const [box, setBox] = useState(0);
 
-    const pages = {
-        0: Vis,
-        1: Hid,
+  const visibilityOfChildComponent = {
+    0: Vis,
+    1: Hid,
+  };
+
+  function hideChild() {
+    if (box === 0) {
+      setBox(1);
+    } else {
+      setBox(0);
     }
+  }
 
-    function hideChild(){
-        // console.log(pages[box]);
-        if(box === 0){
-            setBox(1);
-        }
-        else{
-            setBox(0);
-        }
-    };
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.main",
+        width: "100%",
+        overflow: "hidden",
+        minheight: 100,
+        ...sx,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Title titleCentered={titleCentered} divider={divider}>
+          {title}
+        </Title>
 
-    return (
-        <Box
-            sx={{
-            backgroundColor: "primary.main",
-            width: "100%",
-            overflow: "hidden",
-            // border: "1px solid white",
-            minheight: 100,
-            ...sx,
-            }}
+        <Button
+          onClick={hideChild}
+          sx={{
+            width: 100,
+          }}
         >
-            <div
-                style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                width: "100%",
-                }}
-            >
-                <Title
-                    titleCentered={titleCentered}
-                    divider={divider}
-                    sx={{
-                    // padding: 2,
-                    }}
-                >
-                    {title}
-                </Title>
+          <img src={visibilityOfChildComponent[box][2]} alt="Press"></img>
+        </Button>
+      </div>
 
-                <Button
-                    onClick={ hideChild }
-                    sx={{
-                    width: 100,
-                    }}
-                >
-                    <img src={pages[box][2]} alt="Press"></img>
-                </Button>
-            </div>
-
-            <div 
-                id="menu-dropdown" 
-                style={{ 
-                    visibility : pages[box][0],
-                    height:  pages[box][1],
-                }}>
-                {children}
-            </div>
-        </Box>
-    );
+      <div
+        id="menu-dropdown"
+        style={{
+          visibility: visibilityOfChildComponent[box][0],
+          height: visibilityOfChildComponent[box][1],
+        }}
+      >
+        {children}
+      </div>
+    </Box>
+  );
 };
 
 export default DropDown;
