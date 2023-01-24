e2984de0ce3ed272d65b22c08a67fbd7f49e232a
AankitP <2001aankit@gmail.com>
2023-01-24 08:33:39 -0600

    changes
    One Working

--- server/src/listeners/api/mod.rs
+++ server/src/listeners/api/mod.rs
@@ -5,6 +5,7 @@ use routes::*;
 
 use warp::Filter;
 use tokio::net::{TcpListener, UnixListener};
+use tokio::sync::broadcast;
 use tokio_stream::wrappers::{UnixListenerStream, TcpListenerStream};
 use futures::future::{Either, join_all};
 
@@ -13,15 +14,39 @@ pub enum ApiListener {
     Unix(UnixListener),
 }
 
+<<<<<<< HEAD
 pub async fn listen_on(listeners: Vec<ApiListener>) {
+=======
+pub async fn listen_api(listeners: Vec<ApiListener>, shutdown_channel: broadcast::Sender<()>) -> Result<(), Box<dyn Error + Sync + Send>>{
+>>>>>>> 0b6625b... Implemented graceful shutdown for DNS and API
     let frontend_routes = root_redirect().or(frontend_filter()).with(warp::log("http::frontend"));
 
     join_all(listeners.into_iter().map(|l| {
         let server = warp::serve(frontend_routes.clone());
 
+        let mut shutdown_rx = shutdown_channel.subscribe();
+
         match l {
+<<<<<<< HEAD
             ApiListener::Tcp(l) => Either::Left(server.run_incoming(TcpListenerStream::new(l))),
             ApiListener::Unix(l) => Either::Right(server.run_incoming(UnixListenerStream::new(l)))
         }
     })).await;
+=======
+            ApiListener::Tcp(l) => server.serve_incoming_with_graceful_shutdown(
+                TcpListenerStream::new(l), 
+                async move {shutdown_rx.recv().await.unwrap_or(());}
+            ).left_future(),
+
+            ApiListener::Unix(l) => server.serve_incoming_with_graceful_shutdown(
+                UnixListenerStream::new(l),
+                async move {shutdown_rx.recv().await.unwrap_or(());}
+            ).right_future()
+        }
+    })).await;
+
+    log::info!("API server shut down successfully");
+
+    Ok(())
+>>>>>>> 0b6625b... Implemented graceful shutdown for DNS and API
 }
--- server/src/listeners/mod.rs
+++ server/src/listeners/mod.rs
@@ -1,2 +1,48 @@
 
+<<<<<<< HEAD
 pub mod api;
+=======
+mod api;
+mod dns;
+
+pub use api::ApiListener;
+pub use dns::DnsListener;
+use tokio::{sync::broadcast};
+use tokio::signal::unix::{signal, SignalKind};
+
+use crate::ErrorBox;
+
+/// Start API and DNS servers on the provided listeners
+pub async fn listen(apiaddrs: Vec<api::ApiListener>, dnsaddrs: Vec<dns::DnsListener>) {
+
+    let (shutdown, _) = broadcast::channel(1);
+
+    if let Err(e) = tokio::try_join!(
+        api::listen_api(apiaddrs, shutdown.clone()),
+        dns::listen_dns(dnsaddrs, shutdown.clone()),
+        wait_for_shutdown(shutdown)
+    ) {
+        log::error!("Server halted due to error: {e:?}");
+    };
+
+}
+
+/// Listens for shutdown signals (either SIGINT or SIGTERM) and broadcasts
+/// a shutdown command on the supplied broadcast channel
+async fn wait_for_shutdown(tx: broadcast::Sender<()>) -> Result<(), ErrorBox> {
+
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+
+    tokio::select! {
+        _ = sigint.recv() => {},
+        _ = sigterm.recv() => {},
+    }
+
+    log::debug!("Shutdown signal received; Attempting graceful shut down");
+
+    tx.send(())?;
+
+    Ok(())
+}
+>>>>>>> 0b6625b... Implemented graceful shutdown for DNS and API
