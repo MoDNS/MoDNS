9ebb587de98ee22bcfeb269f6fd8fc7c91658a9e
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-02-14 15:17:50 -0600

    Clean up sdk build script

--- modns-sdk/build.rs
+++ modns-sdk/build.rs
@@ -7,16 +7,20 @@ use std::path::PathBuf;
 fn main() {
     let crate_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    cbindgen::Builder::new()
+    let defaults = cbindgen::Builder::new()
         .with_crate(&crate_dir)
+        .with_namespace("modns_sdk")
+        .with_include_guard("MODNS_SDK_H")
+        .with_autogen_warning("/* Generated using cbindgen from the modns-sdk rust crate. Do not modify manually */");
+
+    defaults.clone()
         .with_language(cbindgen::Language::C)
         .generate()
         .expect("Unable to generate C header file")
         .write_to_file(crate_dir.join("headers/modns-sdk.h"));
 
-    cbindgen::Builder::new()
-        .with_crate(&crate_dir)
-        .with_namespace("modns_sdk")
+    defaults
+        .with_language(cbindgen::Language::Cxx)
         .generate()
         .expect("Unable to generate C++ header file")
         .write_to_file(crate_dir.join("headers/modns-sdk.hpp"));
