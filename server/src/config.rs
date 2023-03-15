7955bcf964aaf96cc801a9f920100177042aae36
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-03-14 23:57:52 -0500

    Flesh out config options

--- server/src/config.rs
+++ server/src/config.rs
@@ -1,7 +1,12 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
+/// A modular DNS resolver
+/// 
+/// MoDNS is a DNS server that uses plugins to provide all functionality
+/// 
+/// This program is the server daemon. If you want to control an already running server, use `modns` instead
 #[derive(Parser)]
 #[command(name = "modnsd")]
 pub struct ServerConfig {
@@ -12,20 +17,36 @@ pub struct ServerConfig {
     /// subdirectories containing a `plugin.so` file and load them
     /// as plugins
     /// 
-    /// Multiple directories can be specified
+    /// Multiple directories can be specified by using -p multiple times
     #[arg(short, long, action=clap::ArgAction::Append)]
     #[arg(default_value=concat!(env!("CARGO_MANIFEST_DIR"), "/../plugins"))]
     plugin_path: Vec<PathBuf>,
 
     /// Path for the Unix Domain socket that is used by the CLI
-    #[arg(short, long, default_value="./modnsd.sock")]
-    socket_path: PathBuf,
+    #[arg(short, long, default_value="/tmp/modnsd.sock")]
+    unix_socket: PathBuf,
 
-    /// Don't immediately exit if the server is initialized into a state that would cause
-    /// errors (ex: if there are no Listeners or Resolvers). Use this option if you want
-    /// to use the CLI or web interface to troubleshoot
-    #[arg(short, long, action=clap::ArgAction::SetTrue)]
-    stay_alive: bool,
+    /// Ignore some, all, or no errors when initially loading plugins
+    /// 
+    /// By default, server will log an error and move on if a plugin fails to
+    /// load, but if all plugins are loaded and the server is unable to handle
+    /// DNS requests (most likely because there isn't a Resolver or Listener
+    /// enabled), server will immediately exit with an error code
+    #[arg(short, long, value_enum, default_value_t)]
+    ignore_init_errors: IgnoreErrorsConfig,
+
+    /// Path to the data directory
+    #[arg(short, long, default_value="./modns-data")]
+    data_dir: PathBuf
+
+}
+
+#[derive(Clone, Copy, ValueEnum, Default)]
+pub enum IgnoreErrorsConfig {
+    Always,
+    #[default]
+    Default,
+    Never
 }
 
 impl ServerConfig {
@@ -33,11 +54,31 @@ impl ServerConfig {
         self.plugin_path.as_ref()
     }
 
-    pub fn socket_path(&self) -> &PathBuf {
-        &self.socket_path
+    pub fn unix_socket(&self) -> &PathBuf {
+        &self.unix_socket
     }
 
-    pub fn stay_alive(&self) -> bool {
-        self.stay_alive
+    pub fn ignore_init_errors(&self) -> IgnoreErrorsConfig {
+        self.ignore_init_errors
+    }
+
+    pub fn strict_init(&self) -> bool {
+        if let IgnoreErrorsConfig::Never = self.ignore_init_errors {
+            true
+        } else {
+            false
+        } 
+    }
+
+    pub fn always_init(&self) -> bool {
+        if let IgnoreErrorsConfig::Always = self.ignore_init_errors {
+            true
+        } else {
+            false
+        } 
+    }
+
+    pub fn data_dir(&self) -> &PathBuf {
+        &self.data_dir
     }
 }
