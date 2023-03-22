cb9edc06ffc2899a0ccf9e81099dcf55b60e03c7
Carter Ray <carterray31001@gmail.com>
2023-03-21 20:52:45 -0500

    Rewrote logic to use:     is_listener, is_interceptor, is_resolver, is_validator, is_inspector instead of the list of implemented modules.

--- frontend/dns_frontend/src/API/getsetAPI.jsx
+++ frontend/dns_frontend/src/API/getsetAPI.jsx
@@ -45,7 +45,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Ad Blocker",                      // set in manifest
                 "description": "Default plugin that blocks ads",    // set in manifest
                 "home": "/opt/modns/plugins/adblock/",              // position in fs
-                "modules": ["interceptor"],                         // list of modules this plugin implements
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": 1,                            // if this plugin implements an interceptor, the position of that interceptor in the execution order
                 "enabled": true                                     // allow plugins to be globally disabled even though they remain installed
             },
@@ -53,14 +57,23 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Default Listener",
                 "description": "Default listener plugin",
                 "home": "/opt/modns/plugins/default-listener",
-                "modules": ["listener"],
+                "is_listener": true,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
             "cd9735cb-5c12-491a-b032-6ccd8cfd6855": {
                 "friendly_name": "DNS Cache",
                 "description": "Default plugin that caches results of previous requests",
                 "home": "/opt/modns/plugins/dnscache",
-                "modules": ["interceptor", "inspector"],
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": true,
                 "intercept_position": 2,
                 "enabled": true
             },
@@ -68,7 +81,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNS over HTTP",
                 "description": "Default plugin that uses http",
                 "home": "/opt/modns/plugins/dohttp",
-                "modules": ["listener", "interceptor", "resolver"],
+                "is_listener": true,
+                "is_interceptor": true,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": 3,
                 "enabled": true
             },
@@ -76,14 +93,23 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNSSEC Validator",
                 "description": "Validates DNS responses using public key cryptography",
                 "home": "/opt/modns/plugins/dnssec/",
-                "modules": ["validator"],
+                "is_listener": false,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": true,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": true
             },
             "667bad3d-5450-453c-a8dc-b2099ba6c4ef": {
                 "friendly_name": "Lan Cache",
                 "description": "Redirects common download URLs to a local caching server",
                 "home": "/opt/modns/plugins/lancache/",
-                "modules": ["interceptor"],
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": null,
                 "enabled": false
             },
@@ -91,14 +117,24 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Secure DNS listener",
                 "description": "Sends DNS request over TLS encrypted channel",
                 "home": "/opt/modns/plugins/dot/",
-                "modules": ["listener"],
+                "is_listener": true,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
             "18897fca-1657-461d-b7d8-90cb5c6fb550": {
                 "friendly_name": "Secure DNS resolver",
                 "description": "Sends DNS request over TLS encrypted channel",
                 "home": "/opt/modns/plugins/dot/",
-                "modules": ["resolver"],
+                "is_listener": false,
+                "is_interceptor": false,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
         }
@@ -109,14 +145,23 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Default Listener",
                 "description": "Default listener plugin",
                 "home": "/opt/modns/plugins/default-listener",
-                "modules": ["listener"],
+                "is_listener": true,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
             "c77818df-750f-417c-9194-d6867ea87ecf": {
                 "friendly_name": "DNS over HTTP",
                 "description": "Default plugin that uses http",
                 "home": "/opt/modns/plugins/dohttp",
-                "modules": ["listener", "interceptor", "resolver"],
+                "is_listener": true,
+                "is_interceptor": true,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": 3,
                 "enabled": true
             },
@@ -124,25 +169,38 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Secure DNS listener",
                 "description": "Sends DNS request over TLS encrypted channel",
                 "home": "/opt/modns/plugins/dot/",
-                "modules": ["listener"],
+                "is_listener": true,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
         }
     } else if (filter === 'interceptor') {
         return {
-            "6c396d8e-9a93-11ed-a8fc-0242ac120002": {
-                "friendly_name": "Ad Blocker",
-                "description": "Default plugin that blocks ads",
-                "home": "/opt/modns/plugins/adblock/",
-                "modules": ["interceptor"],
-                "intercept_position": 1,
-                "enabled": true,
+            "6c396d8e-9a93-11ed-a8fc-0242ac120002": {     // unique id to use in api calls related to this plugin.
+                "friendly_name": "Ad Blocker",                      // set in manifest
+                "description": "Default plugin that blocks ads",    // set in manifest
+                "home": "/opt/modns/plugins/adblock/",              // position in fs
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": 1,                            // if this plugin implements an interceptor, the position of that interceptor in the execution order
+                "enabled": true                                     // allow plugins to be globally disabled even though they remain installed
             },
             "cd9735cb-5c12-491a-b032-6ccd8cfd6855": {
                 "friendly_name": "DNS Cache",
                 "description": "Default plugin that caches results of previous requests",
                 "home": "/opt/modns/plugins/dnscache",
-                "modules": ["interceptor", "inspector"],
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": true,
                 "intercept_position": 2,
                 "enabled": true
             },
@@ -150,7 +208,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNS over HTTP",
                 "description": "Default plugin that uses http",
                 "home": "/opt/modns/plugins/dohttp",
-                "modules": ["listener", "interceptor", "resolver"],
+                "is_listener": true,
+                "is_interceptor": true,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": 3,
                 "enabled": true
             },
@@ -158,7 +220,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Lan Cache",
                 "description": "Redirects common download URLs to a local caching server",
                 "home": "/opt/modns/plugins/lancache/",
-                "modules": ["interceptor"],
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": null,
                 "enabled": false
             },
@@ -169,7 +235,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNS over HTTP",
                 "description": "Default plugin that uses http",
                 "home": "/opt/modns/plugins/dohttp",
-                "modules": ["listener", "interceptor", "resolver"],
+                "is_listener": true,
+                "is_interceptor": true,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
                 "intercept_position": 3,
                 "enabled": true
             },
@@ -177,7 +247,12 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "Secure DNS resolver",
                 "description": "Sends DNS request over TLS encrypted channel",
                 "home": "/opt/modns/plugins/dot/",
-                "modules": ["resolver"],
+                "is_listener": false,
+                "is_interceptor": false,
+                "is_resolver": true,
+                "is_validator": false,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": false
             },
         }
@@ -187,7 +262,12 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNSSEC Validator",
                 "description": "Validates DNS responses using public key cryptography",
                 "home": "/opt/modns/plugins/dnssec/",
-                "modules": ["validator"],
+                "is_listener": false,
+                "is_interceptor": false,
+                "is_resolver": false,
+                "is_validator": true,
+                "is_inspector": false,
+                "intercept_position": null,
                 "enabled": true
             },
         }
@@ -197,7 +277,11 @@ export const getPluginDict = (filter) => {
                 "friendly_name": "DNS Cache",
                 "description": "Default plugin that caches results of previous requests",
                 "home": "/opt/modns/plugins/dnscache",
-                "modules": ["interceptor", "inspector"],
+                "is_listener": false,
+                "is_interceptor": true,
+                "is_resolver": false,
+                "is_validator": false,
+                "is_inspector": true,
                 "intercept_position": 2,
                 "enabled": true
             },
