abf4f4969b450ad4f485285d7d216e0172e2c9e2
AankitP <2001aankit@gmail.com>
2023-03-23 14:53:54 -0500

    Changes
    - Dashboard not empty, just testing tho
    - piechart component made
    - progress thing component made
    - temp data made
    - that is all I can remember
    - Now to mock up a dashboard to use as a template to follow

---
+++ frontend/dns_frontend/src/Components/DashboardComponents/ProgressCircle.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+
+const ProgressCircle = ({ progress = ".75", size = "40"}) => {
+
+    const angle = progress * 360;
+
+    return(
+        <Box
+        
+            sx={{
+                background: `radial-gradient(red 55%, transparent 56%),
+                    conic-gradient(transparent 0deg ${angle}deg, green ${angle}deg 360deg),
+                    blue`,
+                borderRadius : "50%",
+                width : `${size}px`,
+                height : `${size}px`
+            }}
+
+        />
+    )
+
+}
+
+export default ProgressCircle;
--- frontend/dns_frontend/src/Pages/Dashboard.jsx
+++ frontend/dns_frontend/src/Pages/Dashboard.jsx
@@ -1,14 +1,73 @@
-import React from 'react';
+import { Box } from "@mui/material";
+import { Button, IconButton, Typography } from "@mui/material";
+import React from "react";
+import MainBox from "../Components/MainBox";
 
-import MainBox from '../Components/MainBox';
+// import PieChart from "../Components/DashboardComponents/PieChart";
+
+// To be removed later
+import { mockTransactions } from "../Tmp/TempData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import ErrorOutline from "@mui/icons-material/ErrorOutline";
+import TrafficIcon from "@mui/icons-material/TrafficOutlined";
+import StatBox from "../Components/DashboardComponents/StatBox";
 
 const Dashboard = () => {
-    return (
-        <MainBox
-            title={"Dashboard"}
+  return (
+    <MainBox title={"Dashboard"}>
+      {/* Grid */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gap="20px"
+      >
+        {/* Row 1 */}
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
         >
-        </MainBox>
-    );
+          <StatBox
+            title="12,316"
+            Subtitle="DNS Requests"
+            progress=".60"
+            increase="+12%"
+            icon={<TrafficIcon />}
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="11,111"
+            Subtitle="DNS Errors"
+            progress=".20"
+            increase="+3%"
+            icon={<ErrorOutline />}
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="22,222"
+            Subtitle="Cookies"
+            progress=".50"
+            increase="+0%"
+            icon={<DownloadOutlinedIcon />}
+          />
+        </Box>
+      </Box>
+    </MainBox>
+  );
 };
 
 export default Dashboard;
