1b83c575678fb1ff9a31ca1ac7b1a0c718a3e957
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-04-03 15:17:18 -0500

    Fix go sdk linking

--- plugins/cache/main.go
+++ plugins/cache/main.go
@@ -3,6 +3,8 @@ package main
 /*
 #include "modns-sdk.h"
 #include <stdlib.h>
+
+typedef const struct DnsMessage bruh;
 */
 import "C"
 import "unsafe"
@@ -26,4 +28,12 @@ func log(level int8, msg string) {
     }
 
     C.free(unsafe.Pointer(&msg_cstr))
+}
+
+//export impl_inspect_resp
+func impl_inspect_resp(req *C.bruh, resp *C.bruh, source C.uchar, state unsafe.Pointer) C.uchar {
+
+    log(2, "Inspector called")
+
+    return 0
 }
