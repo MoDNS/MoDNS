e6fb06502ab97bef18f2de234b768a7e39614e7c
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-04-03 23:59:50 -0500

    Add executor tests

--- server/tests/modules.rs
+++ server/tests/modules.rs
@@ -1,5 +1,6 @@
 
-use modnsd::plugins::manager::PluginManager;
+use modns_sdk::types::ffi;
+use modnsd::plugins::{manager::PluginManager, ResponseSource};
 
 fn init_logger() {
     let _ = env_logger::builder()
@@ -25,4 +26,51 @@ fn load_test_plugin() {
     assert_eq!(pm.num_validators(), 1, "The PluginManager didn't report having a validator");
 
     assert_eq!(pm.num_inspectors(), 1, "The PluginManager didn't report having an inspector");
+}
+
+#[test]
+fn test_plugin_intercept() {
+
+    init_logger();
+
+    let mut pm = PluginManager::new();
+
+    pm.load("./tests/test-plugin", true).expect("Test plugin failed to load");
+    pm.init().unwrap();
+
+    let message = Box::new(ffi::DnsMessage::default());
+
+    pm.poll_interceptors(&message).expect("Interceptor failed");
+}
+
+#[test]
+fn test_plugin_validate() {
+
+    init_logger();
+
+    let mut pm = PluginManager::new();
+
+    pm.load("./tests/test-plugin", true).expect("Test plugin failed to load");
+    pm.init().unwrap();
+
+    let req = Box::new(ffi::DnsMessage::default());
+    let resp = Box::new(ffi::DnsMessage::default());
+
+    pm.poll_validators(&req, &resp).expect("Validator failed");
+}
+
+#[test]
+fn test_plugin_inspect() {
+
+    init_logger();
+
+    let mut pm = PluginManager::new();
+
+    pm.load("./tests/test-plugin", true).expect("Test plugin failed to load");
+    pm.init().unwrap();
+
+    let req = Box::new(ffi::DnsMessage::default());
+    let resp = Box::new(ffi::DnsMessage::default());
+
+    pm.notify_inspectors(&req, &resp, ResponseSource::Resolver);
 }
