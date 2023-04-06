d0a828bd14bf62fcdb29767efb6760d077ba63f5
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-04-06 12:04:37 -0500

    Fix test plugin

--- server/tests/test-plugin/plugin.c
+++ server/tests/test-plugin/plugin.c
@@ -3,19 +3,19 @@
 #include <stdint.h>
 
 uint8_t impl_intercept_req(const struct DnsMessage *req, struct DnsMessage *resp, void *state) {
-	modns_log_info_ptr("interceptor called", 19);
+	modns_log_cstr(2, "interceptor called");
 
 	return 0;
 }
 
 uint8_t impl_validate_resp(const struct DnsMessage *req, const struct DnsMessage *resp, struct DnsMessage *err_resp, void *state) {
-	modns_log_info_ptr("validator called", 17);
+	modns_log_cstr(2, "validator called");
 
 	return 0;
 }
 
 uint8_t impl_inspect_resp(const struct DnsMessage *req, const struct DnsMessage *resp, uint8_t src, void *state) {
-	modns_log_info_ptr("inspector called", 17);
+	modns_log_cstr(2, "inspector called");
 
 	return 0;
 }
