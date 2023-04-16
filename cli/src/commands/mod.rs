66918bfdcb2a9e5c3f9fbcdd92d4f1bf645932e6
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-04-16 17:55:27 -0500

    Flesh out server config get command

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1,8 +1,12 @@
 
+use std::collections::HashMap;
+
 use crate::{CliOptions, util::make_request};
 
 use anyhow::{Result, Context};
 use hyper::{StatusCode, Method};
+use modnsd::config::MutableConfigValue;
+use serde_json::Value;
 
 pub mod plugins;
 
@@ -11,9 +15,24 @@ pub fn get_config(keys: &[String], config: &CliOptions) -> Result<()> {
     let resp = make_request(Method::GET, &format!("/api/server/config?{}", keys.join("&")), None, None, config)
         .context("Unable to send request")?;
 
+    if resp.status() == StatusCode::NOT_FOUND {
+        anyhow::bail!(resp.body().to_owned())
+    }
+
     if resp.status() != StatusCode::OK {
         anyhow::bail!("Got error code from daemon: {} ({})", resp.status(), resp.body());
     };
+
+    if config.verbose() > 2 {
+        eprintln!("Got response from server: {}", resp.body())
+    }
+
+    let values: HashMap<String, MutableConfigValue<Value>> = serde_json::from_str(resp.body())
+        .context("Couldn't decode server response")?;
+
+    for (key, value) in values {
+        println!("{} = {}{}", key, value.value(), if value.is_overridden() {"*"} else {""})
+    }
 
     Ok(())
 }
