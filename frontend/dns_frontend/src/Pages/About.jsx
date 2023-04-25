64224e9cc221265a16dc050e5cd4399fbb31a1cd
AankitP <2001aankit@gmail.com>
2023-04-25 15:41:56 -0500

    changes
    Removed images

--- frontend/dns_frontend/src/Pages/About.jsx
+++ frontend/dns_frontend/src/Pages/About.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import AboutMe from "../Components/AboutMe/AboutMe";
 import MainBox from "../Components/MainBox";
-import Seth from "../images/SethHS.jpg";
-import Aankit from "../images/AankitHS.JPG";
-import Carter from "../images/CarterHS.jpg";
-import Bronson from "../images/BronsonHS.jpg";
+
 
 const About = () => {
   
