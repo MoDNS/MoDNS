8a89dee858a375c9e96fd1a03f9969c83f6c2bc7
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-04-27 11:38:04 -0500

    WIP: Fix TCP listener

--- plugins/base-listener/src/plugin.c
+++ plugins/base-listener/src/plugin.c
@@ -63,6 +63,10 @@ uint8_t impl_listener_sync_encode_resp(const struct DnsMessage *resp, struct Byt
 
 uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const void *state_ptr) {
 
+    if (state_ptr == NULL) {
+        return 2;
+    }
+
     PluginState *state = (PluginState *)state_ptr;
 
     struct pollfd pollfds[state->num_connections + 2];
@@ -82,25 +86,29 @@ uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const
     int events = poll(pollfds, num_polls, -1);
 
     if (events < 0) {
-        modns_log_cstr(0, "Got error while polling");
-        modns_log_cstr(3, strerror(errno));
+        modns_log(0, 255, "Got error while polling: %s", strerror(errno));
         return 1;
     }
 
-    if (pthread_mutex_trylock(&state->connections_lock) != 0) {
+    if (events > 0) {
+        modns_log(0, 255, "Got %d events, %d connections are active", events, state->num_connections);
+    }
+
+    int lock_rc = pthread_mutex_trylock(&state->connections_lock);
+    if (lock_rc < 0) {
+
+        modns_log(4, 255, "Couldn't acquire mutex lock on TCP connections: %s (rc: %d)", strerror(errno), lock_rc);
+
         if (errno == EBUSY) {
             return 1;
         }
 
-        modns_log(0, 255, "Attempt to acquire lock on TCP connections failed: ", strerror(errno));
+        modns_log(0, 255, "Attempt to acquire lock on TCP connections failed: %s", strerror(errno));
         return 2;
     }
 
     // Handle messages from open TCP connections
-    //
-    // Number and order of active connections may change, so we need to keep track
-    // of our position in list of connections (i) and list of poll results (j)
-    for (int i,j = 0; i < state->num_connections && j < num_polls - 2; i++,j++) {
+    for (uint8_t i = 0; i < state->num_connections; i++) {
 
         if (events == 0) {
             pthread_mutex_unlock(&state->connections_lock);
@@ -108,12 +116,30 @@ uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const
         }
 
         
-        if(pollfds[j].revents) {
+        if(pollfds[i].revents == 0) {
+
+            modns_log(4, 40, "No events on connection %d", i);
             continue;
         }
 
+        modns_log(4, 250, "Got event on connection %d (fd %d): 0x%04X", i, pollfds[i].fd, pollfds[i].revents);
+
         // We've found one of the events, so decrement the counter of events to handle
         events--;
+
+        if (pollfds[i].revents == POLLNVAL) {
+            modns_log(1, 40, "TCP connection at file descriptior %d closed unexpectedly", pollfds[i].fd);
+            return 2;
+        }
+
+        if(pollfds[i].revents != POLLIN) {
+            modns_log(1, 65, "Got unexpected event from polling a TCP socket: 0x%04X", pollfds[i].revents);
+            end_connection(state->connections + i);
+            state->connections[i--] = state->connections[--state->num_connections];
+            pthread_mutex_unlock(&state->connections_lock);
+            return 2;
+
+        }
 
         // First, get the size if we don't have it
         if (state->connections[i].size == 0) {
@@ -129,17 +155,19 @@ uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const
 
                 // Close the connection and remove it from the list of open connections
                 end_connection(state->connections + i);
-                state->connections[i--] = state->connections[state->num_connections--];
-                continue;
+                state->connections[i--] = state->connections[--state->num_connections];
+                pthread_mutex_unlock(&state->connections_lock);
+                return 1;
             }
 
             if (rc != 2) {
-                modns_log(0, 256, "Got unexpected message length while reading message length from a TCP socket: %s", strerror(errno));
+                modns_log(0, 256, "Got unexpected message length while reading message length from a TCP socket: %d", rc);
 
                 // Close the connection and remove it from the list of open connections
                 end_connection(state->connections + i);
-                state->connections[i--] = state->connections[state->num_connections--];
-                continue;
+                state->connections[i--] = state->connections[--state->num_connections];
+                pthread_mutex_unlock(&state->connections_lock);
+                return 1;
             }
 
             state->connections[i].size = ntohs(size);
@@ -158,8 +186,9 @@ uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const
 
             // Close the connection and remove it from the list of open connections
             end_connection(state->connections + i);
-            state->connections[i--] = state->connections[state->num_connections--];
-            continue;
+            state->connections[i--] = state->connections[--state->num_connections];
+            pthread_mutex_unlock(&state->connections_lock);
+            return 1;
         }
 
         state->connections[i].recvd += rc;
@@ -175,103 +204,118 @@ uint8_t impl_listener_async_poll(struct DnsMessage *req, void **req_state, const
             if (decode_bytes(msg_bytes, req) != 0) {
                 modns_log_cstr(1, "Failed to decode a request");
                 end_connection(state->connections + i);
-                state->connections[i--] = state->connections[state->num_connections--];
-                continue;
+                state->connections[i--] = state->connections[--state->num_connections];
+                pthread_mutex_unlock(&state->connections_lock);
+                return 1;
             }
 
             // Pop the connection off and store it in `request_state`
             RequestState *responder = malloc(sizeof(RequestState));
             responder->tcp = true;
-            responder->tcp_conn = state->connections[i];
-            state->connections[i--] = state->connections[state->num_connections--];
+            responder->tcp_conn.sock = state->connections[i].sock;
+            free(state->connections[i].buf);
+            responder->tcp_conn.buf = NULL;
+            responder->tcp_conn.size = 0;
+            responder->tcp_conn.recvd = 0;
+            state->connections[i--] = state->connections[--state->num_connections];
 
             *req_state = responder;
 
             pthread_mutex_unlock(&state->connections_lock);
             return 0;
         }
+    }
 
-        pthread_mutex_unlock(&state->connections_lock);
+    pthread_mutex_unlock(&state->connections_lock);
 
-        if (events == 0) {
+    if (events == 0) {
+        return 1;
+    }
+
+    // Handle incoming UDP stream
+    if (pollfds[num_polls-2].revents) {
+
+        modns_log_cstr(3, "Got event on UDP");
+
+        if (pollfds[num_polls-2].revents != POLLIN) {
+            modns_log(0, 64, "UDP Listener encountered an error (poll returned %d)", pollfds[num_polls-2].revents);
+            return 2;
+        }
+
+        struct sockaddr addr;
+        socklen_t addr_len = sizeof(addr);
+        ssize_t rc = recvfrom(state->udplistener, state->udp_buf, state->buffer_len, MSG_DONTWAIT, &addr, &addr_len);
+
+        if (rc < 0 && !(errno == EWOULDBLOCK || errno == EAGAIN)) {
+            modns_log(0, 255, "Got error reading from UDP listener: %s", strerror(errno));
+            return 2;
+        }
+
+        if (rc == 0) {
+            modns_log_cstr(0, "UDP listener closed unexpectedly");
+            return 2;
+        }
+
+        if (rc > 0) {
+            ByteVector req_bytes;
+            req_bytes.ptr = state->udp_buf;
+            req_bytes.size = rc;
+            req_bytes.capacity = state->buffer_len;
+
+            if (decode_bytes(req_bytes, req) == 0) {
+                RequestState *responder = malloc(sizeof(RequestState));
+                responder->tcp = false;
+                responder->udp_conn.addr = addr;
+                responder->udp_conn.addr_len = addr_len;
+
+                *req_state = responder;
+
+                return 0;
+            } else {
+                modns_log_cstr(1, "Failed to decode a request");
+            }
+
+        }
+
+    }
+
+    // Handle incoming TCP connections
+    if (pollfds[num_polls-1].revents) {
+
+        modns_log_cstr(3, "Got event on TCP");
+
+        if (pollfds[num_polls-1].revents != POLLIN) {
+            modns_log(0, 64, "TCP Listener encountered an error (poll returned %d)", pollfds[num_polls-1].revents);
+            return 2;
+        }
+
+        pthread_mutex_lock(&state->connections_lock);
+        int conn_sock = accept(state->tcplistener, NULL, NULL);
+
+        if (conn_sock < 0) {
+            modns_log(0, 255, "Failed to accept a TCP connection: %s", strerror(errno));
+            pthread_mutex_unlock(&state->connections_lock);
+            return 2;
+        }
+
+        if (state->num_connections >= state->max_connections) {
+            close(conn_sock);
+            modns_log_cstr(1, "A TCP connection was dropped because the maximum number of open connections has been reached");
+            pthread_mutex_unlock(&state->connections_lock);
             return 1;
         }
 
-        // Handle incoming UDP stream
-        if (pollfds[num_polls-2].revents) {
+        TcpConnection new_conn;
+        new_conn.sock = conn_sock;
+        new_conn.buf = NULL;
+        new_conn.size = 0;
+        new_conn.recvd = 0;
 
-            if (pollfds[num_polls-2].revents != POLLIN) {
-                modns_log(0, 64, "UDP Listener encountered an error (poll returned %d)", pollfds[state->num_connections].revents);
-                return 2;
-            }
+        state->connections[state->num_connections++] = new_conn;
+        pthread_mutex_unlock(&state->connections_lock);
 
-            struct sockaddr addr;
-            socklen_t addr_len = 0;
-            ssize_t rc = recvfrom(state->udplistener, state->udp_buf, state->buffer_len, MSG_DONTWAIT, &addr, &addr_len);
+        modns_log(4, 100, "TCP socket at fd %d added to connection queue, there are now %d open connections", new_conn.sock, state->num_connections);
 
-            if (rc < 0 && !(errno == EWOULDBLOCK || errno == EAGAIN)) {
-                modns_log(0, 255, "Got error reading from UDP listener: %s", strerror(errno));
-                return 2;
-            }
-
-            if (rc == 0) {
-                modns_log_cstr(0, "UDP listener closed unexpectedly");
-                return 2;
-            }
-
-            if (rc > 0) {
-                ByteVector req_bytes;
-                req_bytes.ptr = state->udp_buf;
-                req_bytes.size = rc;
-                req_bytes.capacity = state->buffer_len;
-
-                if (decode_bytes(req_bytes, req) == 0) {
-                    RequestState *responder = malloc(sizeof(RequestState));
-                    responder->tcp = false;
-                    responder->udp_conn.addr = addr;
-                    responder->udp_conn.addr_len = addr_len;
-
-                    *req_state = responder;
-
-                    return 0;
-                } else {
-                    modns_log_cstr(1, "Failed to decode a request");
-                }
-
-            }
-
-        }
-
-        // Handle incoming TCP connections
-        if (pollfds[num_polls-1].revents) {
-
-            if (pollfds[num_polls-1].revents != POLLIN) {
-                modns_log(0, 64, "TCP Listener encountered an error (poll returned %d)", pollfds[state->num_connections].revents);
-                return 2;
-            }
-
-            int conn_sock = accept(state->tcplistener, NULL, NULL);
-
-            if (conn_sock < 0) {
-                modns_log(0, 255, "Failed to accept a TCP connection: %s", strerror(errno));
-                return 2;
-            }
-
-            if (state->num_connections >= state->max_connections) {
-                close(conn_sock);
-                modns_log_cstr(1, "A TCP connection was dropped because the maximum number of open connections has been reached");
-                return 1;
-            }
-
-            TcpConnection new_conn;
-            new_conn.sock = conn_sock;
-            new_conn.buf = NULL;
-            new_conn.size = 0;
-            new_conn.recvd = 0;
-
-            state->connections[state->num_connections++] = new_conn;
-
-        }
     }
 
     return 1;
@@ -295,17 +339,27 @@ uint8_t impl_listener_async_respond(const struct DnsMessage *resp, void *req_sta
     }
 
     if (req_state->tcp) {
+        modns_log_cstr(3, "Sending response over TCP");
+        modns_log(4, 35, "Sending response over fd %d", req_state->tcp_conn.sock);
         ssize_t rc = send(req_state->tcp_conn.sock, resp_bytes.ptr, resp_bytes.size, 0);
 
         if (rc < 0) {
-            modns_log(1, 255, "Encountered an error sending a reply: %s", strerror(errno));
+            modns_log(1, 255, "Encountered an error sending a reply over TCP: %s", strerror(errno));
             drop_char_vec(resp_bytes);
+            end_connection(&req_state->tcp_conn);
             return 1;
         }
 
+        modns_log(3, 50, "Successfully sent %d-byte reply over TCP fd %d", rc, req_state->tcp_conn.sock);
+
         req_state->tcp_conn.recvd = 0;
         req_state->tcp_conn.size = 0;
+        free(req_state->tcp_conn.buf);
+        req_state->tcp_conn.buf = NULL;
 
+        free(req_state);
+
+        // Return this connection to the active connection list, in case it sends another request
         pthread_mutex_lock(&state->connections_lock);
         state->connections[state->num_connections++] = req_state->tcp_conn;
         pthread_mutex_unlock(&state->connections_lock);
@@ -315,7 +369,7 @@ uint8_t impl_listener_async_respond(const struct DnsMessage *resp, void *req_sta
         ssize_t rc = sendto(state->udplistener, resp_bytes.ptr, resp_bytes.size, 0, &req_state->udp_conn.addr, req_state->udp_conn.addr_len);
 
         if (rc < 0) {
-            modns_log(1, 255, "Encountered an error sending a reply: %s", strerror(errno));
+            modns_log(1, 255, "Encountered an error sending a reply over UDP: %s", strerror(errno));
             drop_char_vec(resp_bytes);
             return 1;
         }
@@ -329,9 +383,9 @@ void *impl_plugin_setup() {
 
     PluginState *state = allocate_state(2);
 
-    int udpfd = socket(AF_INET, SOCK_STREAM, 0);
+    int udpfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    int tcpfd = socket(AF_INET, SOCK_DGRAM, 0);
+    int tcpfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in tcp_addr, udp_addr;
 
@@ -356,7 +410,7 @@ void *impl_plugin_setup() {
     }
 
     if (listen(tcpfd, 5) < 0) {
-        modns_log_cstr(0, "Failed to listen on TCP listener");
+        modns_log(0, 255, "Failed to listen on TCP listener: %s", strerror(errno));
         free_state(state);
         return NULL;
     }
