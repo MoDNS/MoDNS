a9c423c703c747cfebbb4a2aac8c82543b4a9eaa
AankitP <2001aankit@gmail.com>
2023-04-29 12:10:09 -0500

    changes
    - Attempts at making this work

--- plugins/base-datacollector/src/DashboardDataCollector.cpp
+++ plugins/base-datacollector/src/DashboardDataCollector.cpp
@@ -6,79 +6,124 @@
 #include <jsoncpp/json/value.h>
 #include <jsoncpp/json/json.h>
 
+/*
+Structure of DNS Message
+    struct DnsMessage {
+        uint16_t id;
+        bool is_response;
+        uint16_t opcode;
+        bool authoritative_answer;
+        bool truncation;
+        bool recursion_desired;
+        bool recursion_available;
+        uint16_t response_code;
+        QuestionVector questions;
+        RRVector answers;
+        RRVector authorities;
+        RRVector additional;
+    };
+*/
+
 bool is_empty(std::ifstream &pFile)
 {
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
 
-extern uint8_t impl_inspect_resp(const struct DnsMessage *req,
-                                 const struct DnsMessage *resp,
-                                 uint8_t source,
-                                 const void *plugin_state)
+extern "C" uint8_t impl_inspect_resp(const modns_sdk::DnsMessage *req,
+                                     const modns_sdk::DnsMessage *resp,
+                                     uint8_t source,
+                                     const void *plugin_state)
 {
 
-    std::cout << req;
-    std::cout << resp;
+    // Create a map to store the user's input
+    Json::Value myMap;
 
-    Json::StyledStreamWriter writer;
+    myMap["id"] = req->id;
+    myMap["is_response"] = req->is_response;
+    myMap["opcode"] = req->opcode;
+    myMap["authoritative_answer"] = req->authoritative_answer;
+    myMap["truncation"] = req->truncation;
+    myMap["recursion_desired"] = req->recursion_desired;
+    myMap["recursion_available"] = req->recursion_available;
+    myMap["response_code"] = req->response_code;
+
     std::list<Json::Value> dataList;
 
-    // // Open the input file stream to read JSON data from the file
-    std::ifstream inputFile("myData.json");
+    // Open the input file stream to read JSON data from the file
+    // std::ifstream inputFile("../myData.json");
 
-    // // Create a JSON object to store the list of maps
-    // Json::Value myData;
+    // Create a JSON object to store the list of maps
+    Json::Value myData;
+/*
+    if (!is_empty(inputFile))
+    {
+        // Read the JSON data from the input file stream
+        inputFile >> myData;
 
-    // if (!is_empty(inputFile))
+        // Close the input file stream
+        inputFile.close();
+
+        // Iterate over the JSON data and add each map to the list
+        for (const auto &map : myData)
+        {
+            dataList.push_back(map);
+        }
+    }
+
+    // Add the map to the list
+    if (dataList.size() <= 1000)
+    {
+        dataList.push_back(myMap);
+    }
+    else
+    {
+        dataList.pop_front();
+        dataList.push_back(myMap);
+    }
+
+    inputFile.close();
+*/
+    std::cout << "\n\n\n";
+
+    // for (auto const &i : dataList)
     // {
-    //     // Read the JSON data from the input file stream
-    //     inputFile >> myData;
-
-    //     // Close the input file stream
-    //     inputFile.close();
-
-    //     // Iterate over the JSON data and add each map to the list
-    //     for (const auto &map : myData)
-    //     {
-    //         dataList.push_back(map);
-    //     }
-    // }
-    // // Create a map to store the user's input
-    // Json::Value myMap;
-
-    // // Add the map to the list
-    // if (dataList.size() <= 10000)
-    // {
-    //     dataList.push_back(myMap);
-    // }
-    // else
-    // {
-    //     dataList.pop_front();
-    //     dataList.push_back(myMap);
+    //     std::cout << i["id"] << std::endl;
     // }
 
     // Open a file stream to write the JSON data to a file
-    std::ofstream file("myData.json");
+    // std::ofstream file("../myData.json");
 
-    // // Create a JSON object to store the list of maps
-    // int index = 0;
-    // for (const auto &map : dataList)
-    // {
-    //     myData[index++] = map;
-    // }
+    // Create a JSON object to store the list of maps
+    int index = 0;
+    for (const auto &map : dataList)
+    {
+        myData[index++] = map;
+    }
+
+    std::cout << "\n\n\n";
+
+    for (auto const &i : myData)
+    {
+        std::cout << i["id"] << std::endl;
+    }
 
     // Write the JSON data to the file
-    // Json::StyledStreamWriter writer;
-    writer.write(file, req);
-    writer.write(file, resp);
+    // Json::StyledStreamWriter dataWriter;
+    // file << dataWriter.write(dataList);
 
     // Close the file stream
-    file.close();
+    // file.close();
 
     std::cout << "Data has been written to myData.json" << std::endl;
 
+    // std::string sStyled = styled.write(myData);
+    // cout << "Styled stream:\n";
+
+    // Json::StyledStreamWriter styledStream;
+    // styledStream.write(std::cout, myData);
+
     std::cout << dataList.size() << std::endl;
 
-    return 1;
+    std::cout << "Printed \n";
+    return 0;
 }
-
